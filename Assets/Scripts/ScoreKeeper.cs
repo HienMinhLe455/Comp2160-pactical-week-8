4a11ae87d3720d96cc5f187ef23d4308df9f0614
HienMinhLe455 <hienminh.le@students.mq.edu.au>
2026-09-16 10:02:06 +1000

    Add ScoreKeeper script (with Coin on cmt)

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public static ScoreKeeper Instance { get; private set; }
+
+    public int CurrentScore { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        //Coin.OnPickup += AddScore;
+    }
+
+    private void OnDisable()
+    {
+        //Coin.OnPickup -= AddScore;
+    }
+
+    private void AddScore(int points)
+    {
+        CurrentScore += points;
+    }
+}
